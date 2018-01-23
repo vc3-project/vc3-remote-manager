1e642ac9db92885bde8ed219f0ed89e3aa14eb5d
Lincoln Bryant <lincolnb@uchicago.edu>
2018-01-23 13:39:57 -0600

    going to abandon the idea of poor man's RPC and just do shell callouts

--- vc3-remote-cluster.py
+++ vc3-remote-cluster.py
@@ -7,6 +7,10 @@ import paramiko
 import logging
 import sys
 import os
+import urllib
+import platform
+import re
+import subprocess
 
 __version__ = "0.1.0" 
 
@@ -21,15 +25,20 @@ class RemoteCluster(object):
     def __init__(self,
                     installdir=None,
                     remotehost=None,
+                    condor_version=None,
+                    condor_repository=None,
                     user=None,
                     lrms=None,
                     loglevel=None
                 ):
         self.installdir = installdir
         self.remotehost = remotehost
+        self.condor_version = condor_version
+        self.condor_repository = condor_repository
         self.user       = user
         self.lrms       = lrms
         self.loglevel   = loglevel
+
         # Setup logging
         self._setup_logging(self.loglevel)
         # setup Paramiko
@@ -48,6 +57,7 @@ class RemoteCluster(object):
         """
 
         self._mkinstalldir()
+        self._unpack_condor()
             
     def status(self):
         """
@@ -113,6 +123,98 @@ class RemoteCluster(object):
         ENABLE_URL_TRANSFERS = False
         EOF
         """
+        
+#    def _remote_function(self, code):
+#        """
+#        poor man's RPC
+#
+#        We can replace this with shell callouts, if desired
+#        """
+#        self.log.debug("running %s" % code)
+#        (stdin,stdout,stderr) = self.ssh.exec_command("python -c '" + code + "'")
+#        output = "".join(stdout.readlines())
+#        error = "".join(stderr.readlines())
+#        return output, error
+        
+
+    def _determine_remote_platform(self):
+        #arch, error = self._remote_function("import platform; print platform.machine()")
+        (stdin, stdout, stderr) = self.ssh.exec_command("uname -p")
+        arch = "".join(stdout.readlines()).rstrip()
+        self.log.debug("Remote architecture is: " + arch)
+        if "x86_64" not in arch:
+            self.log.error("Only x86_64 architecture is supported")
+            raise Exception
+
+#        distro_name = platform.linux_distribution()[0]
+#        distro_major = platform.linux_distribution()[1].split(".",1)[0]
+#
+#        if platform.system() == 'Linux':
+#            if any(map( lambda(pat): re.search(pat,distro_name, re.I), ["red ?hat", "scientific", "centos"])):
+#                distro = "RedHat" + distro_major
+#            elif distro_name in ["Debian"]:
+#                distro = "Debian" + distro_major
+#            elif distro_name in ["Ubuntu"]:
+#                distro = "Ubuntu" + distro_major
+#            else:
+#                raise Exception("Unable to determine distro")
+#        elif platform.system() == 'Darwin':
+#                distro = 'MacOSX' # why not?
+#
+        return arch, distro
+
+    def _unpack_condor(self):
+        """
+        Download and unpack a Condor tarball to setup BLAHP and CondorG on the remote side
+        """
+        arch, distro = self._determine_remote_platform()
+
+        condor_platform = "condor-%s-%s_%s-stripped" % (self.condor_version,
+                                                        arch, distro)
+
+        tarball_name = condor_platform + ".tar.gz"
+
+        # temporary
+        self.tmpdir = "/tmp/.cache/condor"
+        self._runcommand("mkdir -p " + self.tmpdir)
+
+        src = self.condor_repository + "/" + tarball_name
+        dest = self.tmpdir + "/" + tarball_name
+
+        self.log.info("Downloading HTCondor tarball")
+        self.log.debug("%s > %s", src, dest)
+
+        try:
+            urllib.urlretrieve(src, dest)
+        except Exception as e:
+            self.log.debug(e)
+            self.log.error("Failed to retrieve the tarball")
+            self.cleanup()
+
+        cmd = "file %s" % dest
+        out = self._runcommand(cmd)
+        if "gzip compressed data" in out:
+            self.log.debug("Filetype is gzip")
+        else:
+            self.log.error("File type is incorrect. Aborting.")
+            self.cleanup()
+ 
+
+    def _runcommand(self, cmd):
+        """
+        Helpful little function to run external *nix commands
+        """
+        self.log.debug("cmd = %s" % cmd)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        (out, err) = p.communicate()
+        out = out.rstrip()
+        if p.returncode == 0:
+            self.log.debug("External command output: %s" % out)
+        else:
+            self.log.error("External command failed: %s" % err)
+            self.cleanup()
+        return out
+
 
     def _setup_logging(self, loglevel):
         """
@@ -141,6 +243,10 @@ if __name__ == '__main__':
         help="Installation path (default: $HOME/.condor/remote)", default="$HOME/.condor/remote")
     parser_install.add_argument("-u", "--user", action="store",
         help="Username on the remote submit host (default: $USER)", default=os.environ['USER'])
+    parser_install.add_argument("-c", "--condor_version", action="store",
+        help="Condor version used for extracting BLAH + Condor-G (default: 8.6.0)", default='8.6.0')
+    parser_install.add_argument("-r", "--condor_repository", action="store",
+        help="URL containing HTCondor tarballs (default: http://download.virtualclusters.org)", default='http://download.virtualclusters.org')
 
     args = parser.parse_args()
 
@@ -152,11 +258,15 @@ if __name__ == '__main__':
         loglevel=20
     else:
         loglevel=30
+       
+    print(args)
 
     if args.which == 'install':
         rc = RemoteCluster(
             installdir=args.installdir,
             remotehost=args.remotehost,
+            condor_version=args.condor_version,
+            condor_repository=args.condor_repository,
             user=args.user,
             lrms=args.lrms,
             loglevel=loglevel
