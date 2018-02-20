f5529738bc3ead98ad9dd15f52aaec1d6c027d49
Lincoln Bryant <lincolnb@uchicago.edu>
2018-02-20 10:36:51 -0600

    added vc3

--- vc3-remote-manager.py
+++ vc3-remote-manager.py
@@ -4,15 +4,18 @@ from __future__ import print_function
 from ftplib import FTP
 from tarfile import TarFile
 
+
 import argparse
 import errno
 import logging
 import os
 import paramiko
 import re
-import sys
 import shutil
+import sys
+import tarfile
 import tempfile
+import textwrap
 
 try:
     from urllib.parse import urlparse
@@ -49,10 +52,24 @@ class SSHManager(object):
         return out, err
         
 
-    def copy(self, recursive=False):
-        """
-        This is a stub
-        """
+    #def put(self, dst, *srcs, recurse=False):
+    #    """
+    #    Accept any number of src files, copy to destination
+    #    """
+    #    try: self.sftp.lstat(dst)
+    #    except:
+    #        self.log.debug("Cannot stat, assuming")
+
+    #    files=[]
+    #    dirs=[]
+    #    for s in srcs:
+    #        if os.path.isdir(s):
+    #            for root, d, f in os.walk(s):
+    #                for file in f:
+    #                    files.append(os.path.join(root,file)) 
+    #                for dir in d:
+    #                    dirs.append(os.path.join(root,dir))
+
 
     def cleanup(self):
         """
@@ -62,13 +79,28 @@ class SSHManager(object):
         self.client.close()
 
 class Bosco(object):
-    def __init__(self, lrms, installdir, version, repository, tag, cachedir):
+    def __init__(self, lrms, version, repository, tag, cachedir, installdir, sandbox):
         self.lrms       = lrms
-        self.installdir = installdir
         self.version    = version
         self.repository = repository
         self.tag        = tag
         self.cachedir   = cachedir
+
+        try:
+            self.installdir = cluster.resolve_path(installdir)
+            log.debug("Installdir is %s" % self.installdir)
+        except:
+            log.warn("Couldn't resolve installdir.. things might not work")
+            self.installdir = installdir
+
+        if sandbox is None:
+            self.sandbox = os.path.join(self.installdir,"sandbox")
+            log.debug("Sandbox directory not specified, defaulting to %s" % self.sandbox)
+        else:
+            self.sandbox = sandbox
+            log.debug("Sandbox directory is %s" % self.sandbox)
+
+        self.etcdir = self.installdir + "/bosco/glite/etc"
         
     def cache_tarballs(self):
         r = urlparse(self.repository)
@@ -138,14 +170,12 @@ class Bosco(object):
                 files = [t.getmember(s) for s in t.getnames() if re.match(match, s)]
                 members.extend(files)
 
-            log.debug("Extracting relevant files...")
+            log.debug("Extracting %s to %s" % (members, tempdir))
             t.extractall(tempdir,members)
 
         # once things are in tmp, we need to need to move things around and
         # make some directories
         dirs = [ 'bosco/glite/etc', 
-                 'bosco/glite/lib', 
-                 'bosco/glite/bin', 
                  'bosco/glite/log', 
                  'bosco/sandbox' ]
         log.debug("Creating BOSCO directories...")
@@ -154,9 +184,9 @@ class Bosco(object):
 
         # list of files and directories that need to move from the extracted tarball to the bosco dir
         to_move = (
-            ['lib','bosco/lib'],
-            ['libexec/glite/bin', 'bosco/bin'],
-            ['sbin/condor_ft-gahp', 'bosco/bin/condor_ft-gahp'] )
+            ['lib','bosco/glite/lib'],
+            ['libexec/glite/bin', 'bosco/glite/bin'],
+            ['sbin/condor_ft-gahp', 'bosco/glite/bin/condor_ft-gahp'] )
 
         for tuple in to_move:
             src = os.path.join(tempdir,cdir,tuple[0])
@@ -166,7 +196,43 @@ class Bosco(object):
             
         log.debug("Deleting old directory: %s " % cdir)
         shutil.rmtree(os.path.join(tempdir,cdir))
-        
+
+        return tempdir
+    
+
+    def create_tarball(self, dst, src):
+        outfile = dst + ".tar.gz"
+        with tarfile.open(outfile, "w:gz") as tar:
+            tar.add(src, arcname=os.path.basename(src))
+        return outfile
+
+
+    def config_ft_gahp(self):
+        #cat >$remote_glite_dir/etc/condor_config.ft-gahp 2>/dev/null <<EOF
+        #BOSCO_SANDBOX_DIR=\$ENV(HOME)/$remote_sandbox_dir
+        #LOG=\$ENV(HOME)/$remote_base_dir_host/glite/log
+        #FT_GAHP_LOG=\$(LOG)/FTGahpLog
+        #SEC_CLIENT_AUTHENTICATION_METHODS = FS, PASSWORD
+        #SEC_PASSWORD_FILE = \$ENV(HOME)/$remote_base_dir_host/glite/etc/passwdfile
+        #USE_SHARED_PORT = False
+        #ENABLE_URL_TRANSFERS = False
+        #EOF
+        config = """\
+            BOSCO_SANDBOX_DIR=%s
+            LOG=%s/glite/log
+            FT_GAHP_LOG=$(LOG)/FTGahpLog
+            SEC_CLIENT_AUTHENTICATION_METHODS = FS, PASSWORD
+            SEC_PASSWORD_FILE = %s/glite/etc/passwdfile
+            USE_SHARED_PORT = False
+            ENABLE_URL_TRANSFERS = False
+        """ % (self.sandbox, self.installdir, self.installdir)
+
+        c = textwrap.dedent(config)
+        cfgfile = os.path.join(self.etcdir,"condor_config.ft-gahp")
+        log.info("Writing HTCondor File Transfer GAHP config file %s" % cfgfile)
+        with ssh.sftp.open(cfgfile, 'wb') as f:
+            f.write(c)
+
 
 class Cluster(object):
     def __init__(self):
@@ -174,44 +240,50 @@ class Cluster(object):
         This is a stub
         """
 
-    def resolve_home(self):
+    def resolve_path(self,path):
         """
-        Determine what $HOME is on the remote side
+        Determine what $HOME or ~ is on the remote side
         """
-        out, _ = ssh.remote_cmd("echo $HOME")
-        log.debug("$HOME is %s" % out)
+        home, _ = ssh.remote_cmd("echo $HOME")
+        log.debug("$HOME is %s" % home)
+
+        if "~" or "$HOME" in path:
+            tokens = path.split("/")[1::]
+            out = os.path.join(home,'/'.join(tokens))
+        else:
+            out = path
 
         return out
         
 
-    def determine_remote_platform(self):
+    def resolve_platform(self):
         """
         Try to identify the remote platform. Currently RH+variants/Debian/Ubuntu
         are supported. A lot of code was lifted from the 'blivet' lib
         """
-        (stdin, stdout, stderr) = self.ssh.exec_command("uname -p")
-        arch = "".join(stdout.readlines()).rstrip()
-        self.log.debug("Remote architecture is: " + arch)
-        if "x86_64" not in arch:
-            self.log.error("Only x86_64 architecture is supported")
-            raise Exception
+        #(stdin, stdout, stderr) = self.ssh.exec_command("uname -p")
+        #arch = "".join(stdout.readlines()).rstrip()
+        #self.log.debug("Remote architecture is: " + arch)
+        #if "x86_64" not in arch:
+        #    self.log.error("Only x86_64 architecture is supported")
+        #    raise Exception
 
         search_path = ['/etc/os-release','/etc/redhat-release']
         for path in search_path:
             try:
-                self.sftp.lstat(path)
+                ssh.sftp.lstat(path)
                 f = path
                 break
             except IOError as e:
                 f = None
-                self.log.debug("Couldn't open %s, continuing.." % path)
+                log.debug("Couldn't open %s, continuing.." % path)
         if f is None:
-            self.log.error("Unknown or unsupported distribution")
+            log.error("Unknown or unsupported distribution")
             sys.exit(1)
 
         if 'os-release' in f:
-            self.log.debug("Parsing os-release")
-            with self.sftp.file(f) as fh:
+            log.debug("Parsing os-release")
+            with ssh.sftp.file(f) as fh:
                     parser = shlex.shlex(f)
                     while True:
                         key = parser.get_token()
@@ -226,15 +298,15 @@ class Cluster(object):
                             parser.get_token()
                             relVer = parser.get_token().strip("'\"")
         elif 'redhat-release' in f:
-            self.log.debug("Parsing redhat-release")
-            with self.sftp.file(f) as fh:
+            log.debug("Parsing redhat-release")
+            with ssh.sftp.file(f) as fh:
                 relstr = fh.readline().strip()
             (product, sep, version) = relstr.partition(" release ")
             if sep:
                 relName = product
                 relVer = version.split()[0].split(".",1)[0]
 
-        self.log.info("Remote distribution and version is: %s %s" % (relName, relVer))
+        log.info("Remote distribution and version is: %s %s" % (relName, relVer))
 
         # assume Linux for now
         if any(map(lambda(p): re.search(p,relName, re.I), ["red ?hat", "scientific", "centos"])):
@@ -244,7 +316,7 @@ class Cluster(object):
         elif relName in ["Ubuntu"]:
             distro = "Ubuntu" + relVer
 
-        return arch, distro
+        return distro
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Install BLAHP and manage remote clusters")
@@ -267,9 +339,18 @@ if __name__ == '__main__':
     parser.add_argument("-b", "--bosco-version", action="store", 
         help="BOSCO version (default 1.2.10)",
         default="1.2.10")
-    parser.add_argument("-c", "--cache-dir", action="store", 
-        help="BOSCO tarball cache dir (default: /tmp/bosco)",
+    parser.add_argument("-c", "--cachedir", action="store", 
+        help="local BOSCO tarball cache dir (default: /tmp/bosco)",
         default="/tmp/bosco")
+    parser.add_argument("-i", "--installdir", action="store", 
+        help="Remote installation directory (default: ~/.condor)",
+        default="~/.condor")
+    parser.add_argument("-t", "--tag", action="store", 
+        help="Request tag hook (default: None)",
+        default=None)
+    parser.add_argument("-s", "--sandbox", action="store", 
+        help="Sandbox directory (default: %installdir/sandbox)",
+        default=None)
 
     args = parser.parse_args()
 
@@ -290,12 +371,62 @@ if __name__ == '__main__':
     log.addHandler(hdlr)
     log.setLevel(loglevel)
 
+    ### the magic happens here ###
 
-    #ssh = SSHManager(args.host, args.port, args.login)
-    #cluster = Cluster()
-    #home = cluster.resolve_home()
-    b = Bosco("condor", "bosco", args.bosco_version, args.repository, "tag", args.cache_dir)
+    """
+    Initialize SSHManager and Cluster classes.
+    """
+    ssh = SSHManager(args.host, args.port, args.login)
+    cluster = Cluster()
+
+    """ 
+    Download platform tarballs, extract bosco components, and transfer them
+    to the remote side
+    """
+
+    log.info("Retrieving BOSCO files...")
+    b = Bosco("condor", args.bosco_version, args.repository, args.tag, args.cachedir, args.installdir, args.sandbox)
     b.cache_tarballs()
-    b.extract_blahp("RedHat6")
-    #ssh.cleanup()
-    
+    distro = cluster.resolve_platform()
+    log.info("Extracting BOSCO files for platform %s" % distro)
+    bdir = b.extract_blahp(distro)
+    if args.tag is not None:
+        tarname = "bosco" + "-" + args.tag
+    else:
+        tarname = "bosco"
+    log.info("Creating new BOSCO tarball for target %s" % args.host)
+    t = b.create_tarball(tarname, os.path.join(bdir,"bosco"))
+
+    src = os.path.join(os.getcwd(),t)
+    dst = cluster.resolve_path(args.installdir + "/" + t)
+    log.info("Transferring %s to %s" % (src, dst))
+    try:
+        ssh.sftp.mkdir(cluster.resolve_path(args.installdir))
+    except IOError as e:
+        log.debug("Couldn't create installdir.. perhaps it already exists?")
+    try: 
+        ssh.sftp.put(src, dst)
+    except Exception as e:
+        log.error("Couldn't transfer %s to %s!" % (src, args.host + ":" + dst))
+        log.debug(e)
+        sys.exit(1)
+
+    log.info("Extracting %s to %s" % ((args.host + ":" + dst),args.installdir))
+    out, err = ssh.remote_cmd("tar -xzf " + dst + " -C " + args.installdir )
+    if err is not '':
+        log.debug(err)
+    log.info("Deleting temporary file %s" % dst)
+    ssh.sftp.remove(dst)
+
+    """ 
+    Add configuration for the site
+    """
+    b.config_ft_gahp()
+
+    """
+    Close any remaining connections and clean up any temporary files
+    """
+    log.info("Terminating SSH connections...")
+    ssh.cleanup()
+    log.info("Deleting temporary file %s" % bdir)
+    shutil.rmtree(bdir)
