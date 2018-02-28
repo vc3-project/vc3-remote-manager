462f36b30650e5add085e07248137bc06599fa75
Lincoln Bryant <lincolnb@uchicago.edu>
2018-02-28 17:25:49 -0600

    we now actually include the package

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from distutils.core import setup
 
 setup(
     name="vc3-remote-manager",
-    version="0.2.0",
+    version="0.3.0",
     description="Tool for installing HTCondor remote GAHP and blahp",
     license='GPL',
     author='VC3 Team',
     author_email='vc3-project@googlegroups.com',
     long_description=open('README.md').read(),
+    install_requires=['paramiko'],
+    packages = ['vc3remotemanager'],
+    scripts = ['scripts/vc3-remote-manager.py'],
     )
