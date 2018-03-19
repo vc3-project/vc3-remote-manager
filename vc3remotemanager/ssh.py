93592b4c8cb2c94d03febc2397061ab2e028584b
Lincoln Bryant <lincolnb@uchicago.edu>
2018-03-19 13:53:58 -0500

    added priv key support

--- vc3remotemanager/ssh.py
+++ vc3remotemanager/ssh.py
@@ -3,16 +3,29 @@ import paramiko
 import sys
 
 class SSHManager(object):
-    def __init__(self, host, port, login):
-        self.login = login
-        self.port  = port
-        self.host  = host
-        self.log   = logging.getLogger(__name__)
+    def __init__(self, host, port, login, keyfile=None):
+        self.login          = login
+        self.port           = port
+        self.host           = host
+        self.log            = logging.getLogger(__name__)
+        self.privatekeyfile = keyfile
+
+        if self.privatekeyfile is not None:
+            try: 
+                k = paramiko.RSAKey.from_private_key_file(self.privatekeyfile)
+            except PasswordRequiredException as e:
+                self.log.debug("Private key seems to have a password.. aborting")
+                raise
+            except IOError as e:
+                self.log.debug("Cannot open private key file.. aborting")
+                raise
+        else:
+            k = None
 
         try:
             self.client = paramiko.SSHClient()
             self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self.client.connect(hostname=self.host,port=self.port,username=self.login)
+            self.client.connect(hostname=self.host,port=self.port,username=self.login,pkey=k)
             self.sftp = self.client.open_sftp()
         except Exception as e:
             self.log.debug(e)
