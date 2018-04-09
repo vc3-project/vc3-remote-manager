ea38e33e8d2d2291f134618847ec1dabbb83b68c
Lincoln Bryant <lincolnb@uchicago.edu>
2018-04-09 10:06:31 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from setuptools import setup
 import os
 
@@ -6,7 +7,7 @@ def package_files(directory):
     paths = []
     for (path, _, filenames) in os.walk(directory):
         for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
+            paths.append(os.path.join(path, filename))
     return paths
 
 extra_files = package_files('patches')
@@ -21,7 +22,7 @@ setup(
     long_description=open('README.md').read(),
     install_requires=['paramiko'],
     packages = ['vc3remotemanager'],
-    package_data={'': extra_files},
+    package_data={'vc3remotemanager': extra_files},
     include_package_data=True,
     scripts = ['scripts/vc3-remote-manager'],
     )
