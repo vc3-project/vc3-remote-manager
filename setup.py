5ff47bc30552e106583de8e8b19f116f7292e783
Lincoln Bryant <lincolnb@uchicago.edu>
2018-04-09 10:19:01 -0500

    apparently we need the ..

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def package_files(directory):
     paths = []
     for (path, _, filenames) in os.walk(directory):
         for filename in filenames:
-            paths.append(os.path.join(path, filename))
+            paths.append(os.path.join('..',path, filename))
     return paths
 
 extra_files = package_files('patches')
