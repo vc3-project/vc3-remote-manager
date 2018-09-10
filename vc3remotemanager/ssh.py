53ec82238785e95518122f227c406f5992940172
Lincoln Bryant <lincolnb@uchicago.edu>
2018-09-10 16:38:59 -0500

    branch for nested ssh

--- vc3remotemanager/ssh.py
+++ vc3remotemanager/ssh.py
@@ -9,8 +9,9 @@ class SSHManager(object):
         self.port           = kwargs.get('port', '22')
         self.host           = kwargs.get('host', None)
         self.privatekeyfile = kwargs.get('keyfile', None) # paramiko defaults to the usual places
+        self.parent         = kwargs.get('parent', None) # Get parent object for nested ssh
         self.log            = logging.getLogger(__name__)
-        
+
         if self.privatekeyfile is not None:
             try:
                 k = paramiko.RSAKey.from_private_key_file(self.privatekeyfile)
@@ -26,8 +27,21 @@ class SSHManager(object):
         try:
             self.client = paramiko.SSHClient()
             self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self.client.connect(hostname=self.host,port=int(self.port),username=self.login,pkey=k)
-            self.sftp = self.client.open_sftp()
+            if self.parent is not None:
+                # Nesting logic here. 
+                pclient = self.parent.client
+                self.log.debug("Opened parent SSHManager object %s", pclient)
+                pclient.set_missing_host_key_policy('paramiko.AutoAddPolicy()')
+                ptransport = pclient.get_transport()
+                self.log.info("Opening nested connection from %s to %s", self.parent.host, self.host)
+                localaddr = (self.parent.host, self.parent.port)
+                destaddr = (self.host, self.port)
+                pchannel = ptransport.open_channel("direct-tcpip", destaddr, localaddr)
+                self.client.connect(hostname=self.host,port=int(self.port),username=self.login,pkey=k,sock=pchannel)
+                self.sftp = self.client.open_sftp()
+            else:
+                self.client.connect(hostname=self.host,port=int(self.port),username=self.login,pkey=k)
+                self.sftp = self.client.open_sftp()
         except Exception as e:
             self.log.debug(e)
             self.log.error("Failed to establish SSH connection")
