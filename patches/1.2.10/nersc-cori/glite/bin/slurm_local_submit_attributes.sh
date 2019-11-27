db68afbc9e384690d4028dea35785d951925bc25
Kenyi Hurtado <khurtado@nd.edu>
2019-11-27 13:47:44 -0500

    Update slurm_local_submit_attributes.sh
    NERSC: Request 4 hours by default

--- patches/1.2.10/nersc-cori/glite/bin/slurm_local_submit_attributes.sh
+++ patches/1.2.10/nersc-cori/glite/bin/slurm_local_submit_attributes.sh
@@ -2,7 +2,7 @@
 
 #echo "#SBATCH --image=docker:opensciencegrid/osgvo-el7:latest"
 #echo "#SBATCH --module=cvmfs"
-echo "#SBATCH -t 02:00:00"
+echo "#SBATCH -t 04:00:00"
 # echo "#SBATCH --partition=shared"
 # echo "#SBATCH --partition=regularx"
 echo "#SBATCH -C haswell"
